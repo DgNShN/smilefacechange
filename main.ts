c4e7f8e674ac4125112ceff51340a038d5d6a102
DGNSHN <33982400+DgNShN@users.noreply.github.com>
2023-02-05 01:54:16 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+basic.showLeds(`
+    . . . . .
+    # . # . .
+    # . # . #
+    # . # . .
+    # # # . .
+    `)
+led.unplot(0, 1)
+basic.pause(500)
+led.unplot(2, 1)
+basic.pause(500)
+led.unplot(0, 2)
+basic.pause(500)
+led.unplot(2, 2)
+basic.pause(500)
+led.unplot(4, 2)
+basic.pause(500)
+led.unplot(2, 3)
+basic.pause(500)
+led.unplot(0, 4)
+basic.pause(500)
+led.plot(1, 1)
+basic.pause(500)
+led.plot(3, 1)
+basic.pause(500)
+led.plot(3, 4)
+basic.pause(500)
+led.plot(4, 3)
